2284675c8aff036bd22ba90e34f7b2c0cba1e513
SDE-JYOTHIR <117816003+SDE-JYOTHIR@users.noreply.github.com>
2023-10-05 15:48:05 +0530

    Add files via upload

---
+++ number.py
@@ -0,0 +1,11 @@
+print("demo of basic data type:number")
+x=3
+y=2.5
+print("x=",x)
+print("y=",y)
+print("datatype of variabe x:",type(x))
+print("datatype of variabe y:",type(y))
+print("Addition:",x+y)
+print("subtraction:",x-y)
+print("multiplication:",x*2)
+print("exponentation:",x**2)
---
+++ string.py
@@ -0,0 +1,13 @@
+print("demo of basic data types:string")
+s="hello"
+t="world"
+print("string1:",s)
+print("string2:",t)
+d=d=s+" "+t
+print("string concatenation:",d)
+print("capitalize:",d.capitalize())
+print("converted to upperase:",s.upper())
+print("right justify a string :",s.rjust(7))
+print("string at center:",s.center(7))
+print("after replacing|with ell:",s.replace('1','(cell)'))
+print("string after stripping leading  and trailing white spaces",'world'.strip())
